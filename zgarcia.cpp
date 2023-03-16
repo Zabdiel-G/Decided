2833b0ff081b2d2a9052f34cd50b38b6d7485f45
root <root@ZabTab>
2023-03-16 14:21:00 -0700

    Z: updated my source file

--- zgarcia.cpp
+++ zgarcia.cpp
@@ -11,3 +11,24 @@ void messageZ()
 {
     cout << "Zabs Feature mode has been Entered" << endl;
 }
+
+/*
+void obstRend()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    //Draw Box
+    for (int i = 0; i < 5; ++i){
+        glPushMatrix();
+        glColor3ub(100, 200, 100);
+        glTranslatef(obst[i].pos[0], obst[i].pos[1], 0.0f);
+        glBegin(GL_QUADS);
+        glVertex2f(-obst[i].w, -obst[i].h);
+        glVertex2f(-obst[i].w, obst[i].h);
+        glVertex2f( obst[i].w, obst[i].h);
+        glVertex2f( obst[i].w, -obst[i].h);
+        glEnd();
+        glPopMatrix();
+    }
+}
+*/
+

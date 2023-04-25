a4a95d0605ce7fb8ee45e8fa5ee30251dbf4fa2a
Zabdiel-G <123495569+Zabdiel-G@users.noreply.github.com>
2023-04-25 09:45:21 -0700

    Add files via upload
    Z: edited source code

--- zgarcia.cpp
+++ zgarcia.cpp
@@ -23,53 +23,31 @@ void messageZ()
     std::cout << "Zabs Feature mode has been Entered" << std::endl;
 }
 
-/*
-void obstRend()
-{
-    int xres = 640;
-    int yres = 480;
-
-    //glClear(GL_COLOR_BUFFER_BIT);
-    //Draw Box
-    for (int i = 0; i < 5; ++i)
-    {
-        glPushMatrix();
-        glColor3f(1.0f, 0.0f, 0.0f);
-        //glTranslatef(obst[i].pos[0], obst[i].pos[1], 0.0f);
-        glTranslatef(xres/2, yres/2, 0.0f);
-        glBegin(GL_QUADS);
-        glVertex2f(-obst[i].w, -obst[i].h);
-        glVertex2f(-obst[i].w, obst[i].h);
-        glVertex2f( obst[i].w, obst[i].h);
-        glVertex2f( obst[i].w, -obst[i].h);
-        glEnd();
-        glPopMatrix();
-    }
-}
-*/
-
-
 void rendEnemR(Asteroid* ahead)
 {
     Asteroid *a = ahead;
     while (a)
     {
         //Log("draw asteroid...\n");
-        glColor3fv(a->color);
+        //glColor3fv(a->color);
         glPushMatrix();
+	glColor3f(1.0f, 0.0f, 0.0f);
         glTranslatef(a->pos[0], a->pos[1], a->pos[2]);
         glRotatef(a->angle, 0.0f, 0.0f, 1.0f);
-        glBegin(GL_LINE_LOOP);
+        //glBegin(GL_LINE_LOOP);
+	glBegin(GL_QUADS);
         //Log("%i verts\n",a->nverts);
         for (int j=0; j<a->nverts; j++)
         {
             glVertex2f(a->vert[j][0], a->vert[j][1]);
         }
         glEnd();
+
         //glBegin(GL_LINES);
-        //  glVertex2f(0,   0);
-        //  glVertex2f(a->radius, 0);
+        //glVertex2f(0,   0);
+        //glVertex2f(a->radius, 0);
         //glEnd();
+	
         glPopMatrix();
         glColor3f(1.0f, 0.0f, 0.0f);
         glBegin(GL_POINTS);

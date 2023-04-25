bec7913b2530b0348bc5abad4cf28b6ebcb96c1b
Zabdiel-G <123495569+Zabdiel-G@users.noreply.github.com>
2023-04-25 09:44:32 -0700

    Add files via upload

---
+++ Global.h
@@ -0,0 +1,37 @@
+//
+//Modified by: Zabdiel Garcia
+//
+
+#include <cstring>
+#include <chrono>
+
+#pragma once
+
+typedef float Flt;
+typedef float Vec[3];
+typedef Flt Matrix[4][4];
+
+using namespace std::chrono;
+
+class Global {
+public:
+        int xres, yres;
+        char keys[65536];
+   // bool serafinFeature;
+    bool obstR;
+    bool dodgePressing;
+    bool swordSlash;
+    bool canPressSword;
+    bool keyPressed;
+        Global() {
+                xres = 640;
+                yres = 480;
+                memset(keys, 0, 65536);
+        //serafinFeature = false;
+        obstR = false;
+        dodgePressing = false;
+        swordSlash = false;
+        canPressSword = true;
+        keyPressed = false;
+        }
+} gl;
